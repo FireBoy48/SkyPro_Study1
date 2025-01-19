ea7eb0031ec070f0c2788ca29dbf7e732c1976a8
FireBoy48 <gert48750@mail.ru>
2025-01-19 10:00:31 +0300

    fixes bags

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -3,9 +3,7 @@ import pytest
 from src.decorators import my_function
 
 with pytest.raises(Exception):
-    print(
-        my_function(2)
-        == "my_function error: my_function() missing 1 required positional argument: 'y'. Inputs: (2,), {}"
-    )
+    assert my_function(2) == "my_function error: my_function() missing 1 required positional argument: 'y'. Inputs: (2,), {}"
+
 
 assert my_function(1, 2) == 3
